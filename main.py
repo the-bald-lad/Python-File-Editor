9394931a5bc3bda2cc77c3715bbadc9fba4b128f
Sam <79758367+the-bald-lad@users.noreply.github.com>
2021-12-16 10:28:35 +0000

    added main function

--- main.py
+++ main.py
@@ -1,23 +1,27 @@
 import os
 os.system("cls")
 
-file_formats = ["txt", "jpeg", "png", "html", "docx", "pptx", "csv", "mp3", "mp4", "m4a", "mid", "wav"]
+def main():
+    file_formats = ["txt", "jpeg", "png", "html", "docx", "pptx", "csv", "mp3", "mp4", "m4a", "mid", "wav"]
 
-folder = input("Enter path to directory: ")
-while True:
-    enter = input("Enter file type to rename: ")
-    if enter not in file_formats:
-        print("Sorry, that file format is not accepted.")
-    else:
-        break
- 
-name = input(f"Enter new name for file type .{enter}: ")
+    folder = input("Enter path to directory: ")
+    while True:
+        enter = input("Enter file type to rename: ")
+        if enter not in file_formats:
+            print("Sorry, that file format is not accepted.")
+        else:
+            break
+    
+    name = input(f"Enter new name for file type .{enter}: ")
 
-for count, filename in enumerate(os.listdir(folder)):
-    temp = filename.split(".")
-    file_ext = temp[1]
-    if file_ext == enter:
-        dst = f"{name} {str(count)}.{file_ext}"
-        src = f"{folder}/{filename}"
-        dst = f"{folder}/{dst}"  
-        os.rename(src, dst)
+    for count, filename in enumerate(os.listdir(folder)):
+        temp = filename.split(".")
+        file_ext = temp[1]
+        if file_ext == enter:
+            dst = f"{name} {str(count)}.{file_ext}"
+            src = f"{folder}/{filename}"
+            dst = f"{folder}/{dst}"  
+            os.rename(src, dst)
+
+if __name__ == "__main__":
+    main()
