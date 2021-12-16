61c7f6585ceae2a4c7f134daac5645ca4d094d38
Sam <79758367+the-bald-lad@users.noreply.github.com>
2021-12-16 16:57:20 +0000

    changed renaming function so different file types can be renamed simultaniously

--- main.py
+++ main.py
@@ -2,26 +2,17 @@ import os
 os.system("cls")
 
 def main():
-    file_formats = ["txt", "jpeg", "png", "html", "docx", "pptx", "csv", "mp3", "mp4", "m4a", "mid", "wav", "py"]
-
     folder = input("Enter path to directory: ")
-    while True:
-        enter = input("Enter file type to rename: ")
-        if enter not in file_formats:
-            print("Sorry, that file format is not accepted.")
-        else:
-            break
-    
-    name = input(f"Enter new name for file type .{enter}: ")
+    print("WARNING, this will rename every file in the given directory!")
+    name = input(f"Enter new name for files: ")
 
     for count, filename in enumerate(os.listdir(folder)):
         temp = filename.split(".")
         file_ext = temp[1]
-        if file_ext == enter:
-            dst = f"{name} {str(count+1)}.{file_ext}"
-            src = f"{folder}/{filename}"
-            dst = f"{folder}/{dst}"  
-            os.rename(src, dst)
+        dst = f"{name} {str(count+1)}.{file_ext}"
+        src = f"{folder}/{filename}"
+        dst = f"{folder}/{dst}"  
+        os.rename(src, dst)
 
 if __name__ == "__main__":
     main()
