2487f4e3ba12a02ffd03890b49cac39cd3d1f897
Sam <79758367+the-bald-lad@users.noreply.github.com>
2021-12-17 11:52:32 +0000

    added support for renaming and changing file ext at the same time

--- main.py
+++ main.py
@@ -1,14 +1,17 @@
 import os 
 import reassign
 import renamer
+import both
 os.system("cls")
 
 def main():
-    ask = input("Bulk rename or reassign[RN/RS]: ").lower()
+    ask = input("Bulk rename, reassign or both[RN/RS/B]: ").lower()
     if ask == "rn":
         renamer.rename()
     elif ask == "rs":
         reassign.reassign()
+    elif ask == "b":
+        both.both()
     else:
         print("Not a valid option. Please try again")
         
